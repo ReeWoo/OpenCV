42c0b4ebfc8d9e8bd06d0ed2498ef995026392a3
ReeWoo <reewoo4674@naver.com>
2022-07-10 14:34:55 +0900

    add image read, show

--- OpenCV/main.cpp
+++ OpenCV/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-
+#include "opencv2/opencv.hpp"
 int main()
 {
-
+	cv::Mat img = cv::imread("C:\\Users\\yoonw\\Downloads\\lena.jpg", cv::IMREAD_GRAYSCALE);
+	cv::imshow("img", img);
+	cv::waitKey(0);
 	return 0;
 }
